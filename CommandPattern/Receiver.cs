4d9ca320f21e3e4efe7a33f16bf2ccef35792134
OlehMorozkoUA <oleh.morozko@gmail.com>
2022-02-02 19:07:59 +0200

    Added Reciever and Action function.

---
+++ CommandPattern/Receiver.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommandConsole.CommandPattern
+{
+    public class Receiver
+    {
+        public void Action()
+        {
+            Console.WriteLine("Called Receiver.Action()");
+        }
+    }
+}
