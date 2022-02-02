85cc915809462ada511f0e9ba8775c0b98d70803
OlehMorozkoUA <oleh.morozko@gmail.com>
2022-02-02 19:08:57 +0200

    Added Concrete Command.

---
+++ CommandPattern/ConcreteCommand.cs
@@ -0,0 +1,15 @@
+﻿namespace CommandConsole.CommandPattern
+{
+    public class ConcreteCommand : Command
+    {
+        public ConcreteCommand(Receiver receiver) : base(receiver)
+        {
+
+        }
+
+        public override void Execute()
+        {
+            receiver.Action();
+        }
+    }
+}
