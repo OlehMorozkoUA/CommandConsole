0ccdb84f342694cd2f1385a6897ecbc116c9e62b
OlehMorozkoUA <oleh.morozko@gmail.com>
2022-02-02 19:08:34 +0200

    Added Invoker.

---
+++ CommandPattern/Invoker.cs
@@ -0,0 +1,17 @@
+﻿namespace CommandConsole.CommandPattern
+{
+    public class Invoker
+    {
+        Command command;
+
+        public void SetCommand(Command command)
+        {
+            this.command = command;
+        }
+
+        public void ExecuteCommand()
+        {
+            command.Execute();
+        }
+    }
+}
