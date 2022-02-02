8b67eb99c4eac9e8fe7ade6e25d9f9c76cdb7ca6
OlehMorozkoUA <oleh.morozko@gmail.com>
2022-02-02 19:09:19 +0200

    Added testing for Command Pattern.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using CommandConsole.CommandPattern;
+using System;
+
+namespace CommandConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Receiver reciever = new Receiver();
+            Command command = new ConcreteCommand(reciever);
+            Invoker invoker = new Invoker();
+
+            invoker.SetCommand(command);
+            invoker.ExecuteCommand();
+        }
+    }
+}
